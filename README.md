00f66fa5a4b62949dc1f5e49c6708136fc21ac5a
Adam Pawliński <adam.pawlinski@gmail.com>
2019-05-17 15:03:19 +0200

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Opis zadania
-
-app prepared to see on github pages or to download and install by typing npm install and then npm build
-routing don't work properly so the detail product page is not showing - will be fixed
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader';
-import App from './containers/App.container';
-import './index.html';
-
-hot(module)(App);
-ReactDOM.render(<App />, document.getElementById('root'));
